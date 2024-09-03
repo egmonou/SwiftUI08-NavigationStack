a92788a0ad4b2e01395937ad2422db5dd30ed020
Nour <mohamednour25@gmail.com>
2024-09-03 20:30:28 +0300

    94

--- NavigationStack/ContentView.swift
+++ NavigationStack/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var age: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+              
+                VStack(alignment: .leading) {
+                    Text("Entr Your Name")
+                    TextField("Ron Erex", text: $name)
+                }
+                VStack(alignment: .leading) {
+                    Text("Entr Your Age")
+                    TextField("16", text: $age)
+                        .keyboardType(.numberPad)
+                }
+                
+                Spacer()
+                
+                NavigationLink(destination: {
+                    DrivingView(name: name, age: age, DirvingAge: 18)
+                }, label: {
+                    Text("Okay")
+                        .font(.title)
+                        .overlay(
+                            Capsule()
+                                .stroke()
+                        )
+                        .padding()
+                })
+                
+                NavigationLink(destination: {
+                    ZStack {
+                        Color.orange.ignoresSafeArea()
+                        Text("New View")
+                    }
+                }, label: {
+                    Text("Go to orange View")
+                        .font(.title)
+                        .overlay(
+                            Capsule()
+                                .stroke()
+                                .fill(Color.orange)
+                        )
+                        .padding()
+                })
+                
+            }
+            .padding()
+            .navigationTitle("Example")
         }
-        .padding()
+    }
+}
+
+struct DrivingView: View {
+    let name: String
+    let age: String
+    let DirvingAge: Int
+    
+    var numericalAge: Int {
+        Int(age) ?? -1
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.blue.opacity(0.7).ignoresSafeArea()
+            if numericalAge >= DirvingAge {
+                Text("\(name.capitalized) you can drive")
+            }else if numericalAge <= 0 {
+                Text("Invalid age")
+            }
+            else {
+                VStack {
+                    Text("Sorry \(name.capitalized)")
+                    Text("You can't drive")
+                }
+            }
+        }
     }
 }
 
